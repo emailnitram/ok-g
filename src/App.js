e2188ba8da86ef5d42bfdfcf5513d92cf06eec2a
Martin Henry <emailnitram@gmail.com>
2016-11-03 11:53:16 -0700

    update to using an iframe

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+/*global SpeechKITT annyang*/
+/*eslint no-undef: "error"*/
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Forecast from 'react-forecast';
 
 
 class App extends Component {
@@ -20,16 +22,13 @@ class App extends Component {
       // Add our commands to annyang
       annyang.addCommands({
         'what\'s the weather in *location': function(location) {
-          console.log('Weather for: ', location);
           // that.setState({city: location})
-          console.log('SpeechKITT', SpeechKITT)
-          fetch(`http://api.opencagedata.com/geocode/v1/json?q=${location}&key=63b4939c29a8c68d4775f6138e28c874`).then(function(response) { 
+          fetch(`http://api.opencagedata.com/geocode/v1/json?q=${location}&key=63b4939c29a8c68d4775f6138e28c874`).then(function(response) {
               // Convert to JSON
               return response.json();
             }).then(function(res) {
               let currentLatLng = res.results[0].geometry
               // Yay, `j` is a JavaScript object
-              // console.log('lat lng: ', j); 
               that.setState({
                 city: location,
                 currentLatLng
@@ -55,23 +54,22 @@ class App extends Component {
 
   }
   render() {
-    console.log('currentLatLng',this.state.currentLatLng)
     if (!this.state.currentLatLng.hasOwnProperty('lat') && !this.state.currentLatLng.hasOwnProperty('lng')) {
       return (
         <div>
-
+          try a new search
         </div>
       )
     }
-    console.log('city', this.state.city)
+    let that = this;
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React {this.state.city}</h2>
         </div>
         <p className="App-intro">
-          <iframe id="forecast_embed" type="text/html" frameborder="0" height="245" width="100%" src={`http://forecast.io/embed/#lat=${this.state.currentLatLng.lat}&lon=${this.state.currentLatLng.lng}&name=${this.state.city}`}> </iframe>
+          <iframe id="forecast_embed" type="text/html" frameBorder="0" height="245" width="100%" key={that.state.city} src={`http://forecast.io/embed/#lat=${that.state.currentLatLng.lat}&lon=${that.state.currentLatLng.lng}&name=${that.state.city}`}> </iframe>
         </p>
       </div>
     );
@@ -79,5 +77,3 @@ class App extends Component {
 }
 
 export default App;
-
-          // <Forecast latitude={this.state.currentLatLng.lat} longitude={this.state.currentLatLng.lng} name={this.state.city} />
